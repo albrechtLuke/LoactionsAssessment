d951abc933fb6689dacd0407bd7e7b2edbb01e0b
Luke Albrecht <lukealbrecht27@gmail.com>
2021-08-02 21:15:23 +1000

    Fix 1.14

--- javascript/navigation.js
+++ javascript/navigation.js
@@ -1,10 +1,15 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
+	
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+	
+	if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-  } else {
+	} 
+	
+	else {
     document.getElementById("navbar").style.top = "-10vmin";
-  }
+	}
+	
   prevScrollpos = currentScrollPos;
 }
